81f93179521cda2902a453d3c05c509ce73b9be2
SystemFall <matheus.lougen@gmail.com>
2022-12-21 10:42:05 -1200

    feat: removed 'user_commands.naosei' and 'user_command.trackip'
    Remove the unused commands /naosei and /rastrearip
    
    BREAKING CHANGES: 'gaymerbot.extensions.user_commands.naosei' and 'gaymerbot.extensions.user_commands.trackip' no longer exist

--- gaymerbot/extensions/user_commands.py
+++ gaymerbot/extensions/user_commands.py
@@ -1,6 +1,5 @@
 
 import time
-import aiohttp
 import discord
 import datetime
 from discord.ext import commands
@@ -38,39 +37,6 @@ class user_commands(commands.Cog):
             await interaction.response.send_message(user.display_avatar)
         else:
             await interaction.response.send_message(interaction.user.display_avatar)
-
-    @app_commands.command(name='naosei', description='sei la kkk')
-    @app_commands.guild_only()
-    async def naosei(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message('Irineu você não sabe nem eu!')
-
-    @app_commands.command(name='rastrearip', description='Rastreia geolocalização de um endereço IP usando a Weather API')
-    @app_commands.describe(ip='Endereço IP do alvo')
-    @app_commands.guild_only()
-    async def trackip(self, interaction: discord.Interaction, ip: str) -> None:
-        # /rastrearip {ip: str}
-        query = f'http://api.weatherapi.com/v1/current.json?key={self.client.config.weather_api_key}&q={ip}&aqi=no'
-        try:
-            async with aiohttp.ClientSession() as cs:
-                async with cs.get(query) as r:
-                    response = await r.json()
-        except Exception as e:
-            self.log.exception(e)
-        else:
-            self.log.info(response)
-            if response['error']:
-                error_message = response['error']['message']
-                await interaction.response.send_message(f'Um erro com a API aconteceu: {error_message}')
-            else:
-                location = response['location']
-                embed = discord.Embed(title='Rastreamento de endereço IP', description=f'Aqui estão os resultados de sua busca para o ip ``{ip}``', color=0x087500)
-                embed.add_field(name='Cidade', value=location['name'], inline=True)
-                embed.add_field(name='Estado', value=location['region'], inline=True)
-                embed.add_field(name='País', value=location['country'], inline=True)
-                embed.add_field(name='Latitude', value=location['lat'], inline=True)
-                embed.add_field(name='Longitude', value=location['lon'], inline=True)
-                embed.add_field(name='Hora local', value=location['localtime'], inline=True)
-                await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 async def setup(client):
