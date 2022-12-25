9718ade5cb497a46d92b6a6ec221af354b585aa0
SystemFall <matheus.lougen@gmail.com>
2022-12-24 14:04:31 -1200

    style: fix typo

--- gaymerbot/extensions/roles.py
+++ gaymerbot/extensions/roles.py
@@ -19,7 +19,7 @@ class Tags(commands.Cog):
         age_embed = discord.Embed(title='🔞 » Idade', description='Qual sua idade ?', colour=discord.Colour.random())
         sexuality_embed = discord.Embed(title='❤ » Orientação sexual', description='Qual sua orientação sexual ?', colour=discord.Colour.random())
         games_embed = discord.Embed(title='🎮 » Jogos', description='Quais jogos você joga ou se interessa ?', colour=discord.Colour.random())
-        notification_embed = discord.Embed(title='🔔 » Notificações', description='´Você deseja receber notificações ?', colour=discord.Colour.random())
+        notification_embed = discord.Embed(title='🔔 » Notificações', description='Você deseja receber notificações ?', colour=discord.Colour.random())
         await interaction.channel.send(embed=furry_embed, view=Furry(self.client))
         await interaction.channel.send(embed=age_embed, view=Age(self.client))
         await interaction.channel.send(embed=sexuality_embed, view=Sexuality(self.client))
