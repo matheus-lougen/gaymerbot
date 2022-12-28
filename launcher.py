40034ed9f40cf5b92e378576ea7a39846f2128eb
SystemFall <matheus.lougen@gmail.com>
2022-12-27 23:44:16 -1200

    feature: new cli arguments handler
    BREAKING CHANGE: Removed argparse cli arguments handler now using click for a better user experience

--- launcher.py
+++ launcher.py
@@ -2,9 +2,9 @@
 
 import os
 import time
+import click
 import logging
 import asyncio
-import argparse
 import platform
 
 import discord
@@ -14,57 +14,43 @@ from gaymerbot.modules import Logger
 from gaymerbot.modules import Config
 
 
-parser = argparse.ArgumentParser(description='Launch the discord bot')
-parser.add_argument("-d", "--debug", type=bool, required=False, help='Enable main logger debug mode')
-parser.add_argument("-dd", "--discorddebug", type=bool, required=False, help='Enable discord logger debug mode')
-parser.add_argument("-dhd", "--discordhttpsdebug", type=bool, required=False, help='Enable discord.https logger debug mode')
-args = parser.parse_args()
-
-# Default all args as False
-if args.debug is None:
-    args.debug = False
-
-if args.discorddebug is None:
-    args.discorddebug = False
-
-if args.discordhttpsdebug is None:
-    args.discordhttpsdebug = False
+def setup_logging(debug):
+    logger = Logger()
+    logger.setup_formatters()
+    log = logger.setup_logger('main', debug_mode=debug, file_level=logging.INFO, stream_level=logging.INFO)
+    logger.setup_logger('commands', debug_mode=debug, file_level=logging.INFO, stream_level=logging.INFO)
+    logger.setup_logger('extensions', debug_mode=debug, file_level=logging.INFO, stream_level=logging.INFO)
+    logger.setup_logger('discord', file_level=logging.INFO, stream_level=logging.INFO)
+    logger.setup_logger('discord.https', file_level=logging.WARNING, stream_level=logging.WARNING)
+    return log
 
 
-logger = Logger()
-logger.setup_formatters()
-log = logger.setup_logger('main', debug_mode=args.debug, file_level=logging.INFO, stream_level=logging.INFO)
-commands_log = logger.setup_logger('commands', debug_mode=args.debug, file_level=logging.INFO, stream_level=logging.INFO)
-extension_log = logger.setup_logger('extensions', debug_mode=args.debug, file_level=logging.INFO, stream_level=logging.INFO)
-discord_log = logger.setup_logger('discord', debug_mode=args.discorddebug, file_level=logging.INFO, stream_level=logging.INFO)
-discord_https_log = logger.setup_logger('discord.https', debug_mode=args.discordhttpsdebug, file_level=logging.WARNING, stream_level=logging.WARNING)
-
-
-@logger.exception_catcher('main')
-def setup(initial_extensions, start_time, config):
+@click.group(invoke_without_command=True)
+@click.option('--debug', is_flag=True, help='Launch with debug mode')
+def main(debug):
+    initial_extensions = (
+        'gaymerbot.extensions.roles',
+        'gaymerbot.extensions.events',
+        'gaymerbot.extensions.Twitch',
+        'gaymerbot.extensions.user_commands',
+        'gaymerbot.extensions.admin_commands',
+        'gaymerbot.extensions.developer_commands'
+    )
+    log = setup_logging(debug)
+    start_time = time.time()
+    config_path = './data/config.yaml'
+    config = Config(config_path)
     log.debug(f'Discord.py version [{discord.__version__}]')
     log.debug(f'Python version [{platform.python_version()}]')
     log.debug(f'OS information [{platform.system()} {platform.release()} ({os.name})]')
     client = Client(start_time, config, initial_extensions)
-    asyncio.run(main(client, config))
-    # Extensions to load when the bot turns on
+    asyncio.run(run_bot(client, config))
 
 
-async def main(client, config):
+async def run_bot(client, config):
     async with client:
         await client.start(config.token)
 
-initial_extensions = (
-    'gaymerbot.extensions.roles',
-    'gaymerbot.extensions.events',
-    'gaymerbot.extensions.user_commands',
-    'gaymerbot.extensions.admin_commands',
-    'gaymerbot.extensions.developer_commands'
-)
 
-# CLient startup timestamp
-start_time = time.time()
-
-config_path = './data/config.yaml'
-config = Config(config_path)
-setup(initial_extensions, start_time, config)
+if __name__ == '__main__':
+    main()
